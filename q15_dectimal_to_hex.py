7674b89cdc25c2cad48921a764f379f3f6b3a228
Yael Livne <t-yalivn@microsoft.com>
2020-10-24 11:26:13 +0300

    fix the program to do correctly.

--- q15_dectimal_to_hex.py
+++ q15_dectimal_to_hex.py
@@ -1,18 +1,10 @@
-def twos_comp(val):
-    if val != (-2**15):
-        return 2 ** 15 - val
-    return ~val + 1
-
-
 def compute_hex(fraction_input):
     if fraction_input > 1 or fraction_input < -1:
         return "input is not a fraction!"
     q15_input = int(fraction_input * (2 ** 15))
-    if q15_input < 0:
-        return hex(twos_comp(q15_input))
-    return hex(q15_input)
+    return hex(q15_input & (2**16 - 1))
 
 
 if __name__ == '__main__':
-    user_input = 0.25
+    user_input = -0.75
     print(compute_hex(user_input))
